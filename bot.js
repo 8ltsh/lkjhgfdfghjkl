0daab59095ad65512388d1b5284f69681c2f7d3a
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-02-09 09:06:00 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('message', msg => {
 client.login(process.env.BOT_TOKEN);
 
 client.on('message', message => {
-    var prefix = "@"
+    var prefix = "#"
   if (message.author.x5bz) return;
   if (!message.content.startsWith(prefix)) return;
 
@@ -51,7 +51,7 @@ client.on('message', message => {
 });//Toxic Codes
 
 client.on('message', message => {
-    var prefix = '@'
+    var prefix = '#'
     if (message.content.startsWith(prefix + "avatar")) {
         var mentionned = message.mentions.users.first();
     var x5bzm;
@@ -69,7 +69,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === ('$bot')) {
+    if (message.content === ('#bot')) {
     message.channel.send({
         embed: new Discord.RichEmbed()
             .setAuthor(client.user.username,client.user.avatarURL)
@@ -86,3 +86,52 @@ client.on('message', message => {
 }
 });
 
+client.on('message', message => {
+   if(!message.channel.guild) return;
+    var success = new Discord.RichEmbed()
+    .setDescription(`تم أرسال رسالتك بنجاح.`)
+    .setColor('GREEN')
+if(message.content.startsWith(prefix + 'bc')) {
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let BcList = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setDescription(`**▶ 📝 لأرسال رسالة امبد قم بالضغط على \n ▶ ✏ لأرسال رسالة عادية قم بالضغط على \n ★ ${user} \n ★ ${server} \n ★ ${seender}**`)
+if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(BcList).then(msg => {
+msg.react('📝')
+.then(() => msg.react('✏'))
+.then(() =>msg.react('📝'))
+ 
+let EmbedBcFilter = (reaction, user) => reaction.emoji.name === '📝' && user.id === message.author.id;
+let NormalBcFilter = (reaction, user) => reaction.emoji.name === '✏' && user.id === message.author.id;
+ 
+let EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
+let NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
+ 
+ 
+EmbedBc.on("collect", r => {
+ 
+message.channel.send(success);
+message.guild.members.forEach(m => {
+let EmbedRep = args.replace('[السيرفر]' ,message.guild.name).replace('[العضو]', m).replace('[المرسل]', `${message.author}`)
+var bc = new
+Discord.RichEmbed()
+.setColor('RANDOM')
+.setDescription(EmbedRep)
+.setFooter(`${prefix}invite | لدعوة برودكاست بوت`)
+m.send({ embed: bc })
+msg.delete();
+})
+})
+NormalBc.on("collect", r => {
+  message.channel.send(success);
+message.guild.members.forEach(m => {
+let NormalRep = args.replace('[السيرفر]' ,message.guild.name).replace('[العضو]', m).replace('[المرسل]', `${message.author}`)
+m.send(NormalRep);
+msg.delete();
+})
+})
+})
+}
+});
