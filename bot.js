512a84aea17a83ecd8c67ac01560db053101b029
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-02-09 08:54:58 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,13 +50,39 @@ client.on('message', message => {
 }
 });//Toxic Codes
 
-client.on('message' , message => {
-  if(message.author.bot) return;
-  if(message.content.startsWith(prefix + "ping")) {
- message.channel.send('pong').then((msg) => {
-var PinG = `${Date.now() - msg.createdTimestamp}`
-var ApL = `${Math.round(client.ping)}`
-      msg.edit(`\`\`\`javascript\nTime taken: ${PinG} ms.\nDiscord API: ${ApL} ms.\`\`\``);
- })
-  }  
- });
+client.on('message', message => {
+    var prefix = '@'
+    if (message.content.startsWith(prefix + "avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+client.on('message', message => {
+    if (message.content === ('$bot')) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .addField('**Bot Ping**🚀 :' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('**Servers**📚 :', [client.guilds.size], true)
+            .addField('**Channels**📝 :' , `[ ${client.channels.size} ]` , true)
+            .addField('**Users**🔮 :' ,`[ ${client.users.size} ]` , true)
+            .addField('**Bot Name**🔰 :' , `[ ${client.user.tag} ]` , true)
+            .addField('**Bot Owner**👑 :' , `[<@486322208109494282>]` , true)
+            .setFooter(message.author.username, message.author.avatarURL)
+    })
+}
+});
+
