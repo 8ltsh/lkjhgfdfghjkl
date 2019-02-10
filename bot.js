226853e7b2f0d0f935c2db6e5b70d420e8744a9e
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-02-10 06:55:21 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -120,62 +120,52 @@ client.on("guildMemberAdd", member => {
 })
 
 
-client.on('message', message => {
-    let prefix = '#';
-if(message.content.startsWith(prefix + "tempmute")){
-    let muteduser = message.mentions.members.first();
-    let jif = message.content.split(' ').slice(1);
-    let durration = jif[1];
-    let reason = message.content.split(' ').slice(3).join(' ');
-    let hh;
-    let muted = message.guild.roles.find(r => r.name === 'Muted');
-    if(!muteduser){
-        return message.channel.send('**#- I cannot find this guy**');
-    }
-    if(!message.guild.me.hasPermission('ADMINISTRATOR')){
-        return message.channel.send(`**#- I must have the \`ADMINISTRATOR\` Perms so i can mute people**`);
-    }
-    if(muteduser.hasPermission('ADMINISTRATOR')) {
-        return message.channel.send(`**#- He has a \`ADMINISTRATOR\` Perms and u cannot mute him**`);
-    }
-    if(!message.member.hasPermission('ADMINISTRATOR')){
-        return message.channel.send('**#- You must have \`ADMINISTRATOR\` Perms.**');
-    }
-    if(muteduser.id === message.author.id){
-        return message.channel.send(`**#- You can't mute yourself**`);
-    }
-    if(durration && !durration.match(/[1,10][s,m,h,d,w]/g)){
-        return message.channel.send('**#- Submit a right durration. \n Must be like the following submitation : 1-10 s = second , m = minute , h = hour , d = days, w = weeks**');
-    }
-    if(!muted){
-        return message.guild.createRole({name: 'Muted'});
-    }
-    if(!reason){
-       hh = null;
-    } else {
-        hh = reason;
-    }
-    if(hh === null){
-        hh = "No reason detected";
-    }
-    message.channel.send(`**${muteduser} Got muted :white_check_mark: \n Durration : ${durration}\n Reason : ${hh}**`);
-    console.log(mms(durration));
-    message.guild.channels.filter(m => m.type === 'text').forEach(f => {
-                      f.overwritePermissions(muted, {
-            SEND_MESSAGES: false
+
+const Discord = require("discord.js");
+const ms = require("ms");
+
+module.exports.run = async (bot, message, args) => {
+
+  //!tempmute @user 1s/m/h/d
+
+  let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!tomute) return message.reply("Couldn't find user.");
+  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
+  let muterole = message.guild.roles.find(`name`, "muted");
+  //start of create role
+  if(!muterole){
+    try{
+      muterole = await message.guild.createRole({
+        name: "muted",
+        color: "#000000",
+        permissions:[]
+      })
+      message.guild.channels.forEach(async (channel, id) => {
+        await channel.overwritePermissions(muterole, {
+          SEND_MESSAGES: false,
+          ADD_REACTIONS: false
         });
-    });
-    message.guild.channels.filter(s => s.type === 'voice').forEach(h => {
-                      h.overwritePermissions(muted, {
-            SPEAK: false
-        });
-    });
-    muteduser.addRole(muted).then(setTimeout(() => {
-    muteduser.removeRole(muted);
-    message.channel.send(`**${muteduser} Finally got unmuted :white_check_mark:**`);
-}, mms(durration)));
- 
-   
- 
+      });
+    }catch(e){
+      console.log(e.stack);
+    }
+  }
+  //end of create role
+  let mutetime = args[1];
+  if(!mutetime) return message.reply("You didn't specify a time!");
+
+  await(tomute.addRole(muterole.id));
+  message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+
+  setTimeout(function(){
+    tomute.removeRole(muterole.id);
+    message.channel.send(`<@${tomute.id}> has been unmuted!`);
+  }, ms(mutetime));
+
+
+//end of module
 }
-});
+
+module.exports.help = {
+  name: "tempmute"
+}
