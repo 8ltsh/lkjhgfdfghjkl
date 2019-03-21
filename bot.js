7e0c2163bafe793ce9aa43e25b0fc65ae33e2edc
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-03-21 09:34:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -121,51 +121,222 @@ client.on("guildMemberAdd", member => {
 
 
 
-const Discord = require("discord.js");
-const ms = require("ms");
 
-module.exports.run = async (bot, message, args) => {
 
-  //!tempmute @user 1s/m/h/d
-
-  let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!tomute) return message.reply("Couldn't find user.");
-  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
-  let muterole = message.guild.roles.find(`name`, "muted");
-  //start of create role
-  if(!muterole){
-    try{
-      muterole = await message.guild.createRole({
-        name: "muted",
-        color: "#000000",
-        permissions:[]
-      })
-      message.guild.channels.forEach(async (channel, id) => {
-        await channel.overwritePermissions(muterole, {
-          SEND_MESSAGES: false,
-          ADD_REACTIONS: false
-        });
-      });
-    }catch(e){
-      console.log(e.stack);
+const ytdl = require("ytdl-core");
+const { Client, Util } = require('discord.js');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const queue = new Map();
+ 
+ 
+ 
+ 
+client.on('message', async msg => {
+  if (msg.author.bot) return undefined;
+  if (!msg.content.startsWith(prefix)) return undefined;
+  const args = msg.content.split(' ');
+  const searchString = args.slice(1).join(' ');
+  const url = args[1] ? args[1] .replace(/<(.+)>/g, '$1') : '';
+  const serverQueue = queue.get(msg.guild.id);
+  let command = msg.content.toLowerCase().split(" ")[0];
+  command = command.slice(prefix.length)
+  if (command === `play`) {
+    const voiceChannel = msg.member.voiceChannel;
+    if (!voiceChannel) return msg.channel.send('يجب توآجد حضرتك بروم صوتي .');
+    const permissions = voiceChannel.permissionsFor(msg.client.user);
+    if (!permissions.has('CONNECT')) {
+      return msg.channel.send('لا يتوآجد لدي صلاحية للتكلم بهذآ الروم');
     }
+    if (!permissions.has('SPEAK')) {
+      return msg.channel.send('لا يتوآجد لدي صلاحية للتكلم بهذآ الروم');
+    }
+ 
+    if (!permissions.has('EMBED_LINKS')) {
+      return msg.channel.sendMessage("**يجب توآفر برمشن `EMBED LINKS`لدي **rl")
+      }
+ 
+    if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+      const playlist = await youtube.getPlaylist(url);
+      const videos = await playlist.getVideos();
+      for (const video of Object.values(videos)) {
+        const video2 = await youtube.getVideoByID(video.id);
+        await handleVideo(video2, msg, voiceChannel, true);
+      }
+      return msg.channel.send(` **${playlist.title}** تم الإضآفة إلى قأئمة التشغيل`);
+    } else {
+      try {
+ 
+        var video = await youtube.getVideo(url);
+ 
+      } catch (error) {
+        try {
+                          var fast = {};
+          var videos = await youtube.searchVideos(searchString, 10);
+          let index = 0;
+          const embed1 = new Discord.RichEmbed()
+              .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
+${videos.map(video2 => `[**${++index}**] **${video2.title}**`).join('\n')}`)
+          .setFooter(`${msg.guild.name}`)
+            .setThumbnail('https://e.top4top.net/p_1001lsv3w1.png')
+ 
+          msg.channel.sendEmbed(embed1).then(message =>{
+ 
+            message.delete(15000)
+ 
+          });
+          try {
+            var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
+              maxMatches: 1,
+              time: 20000,
+              errors: ['time']
+            })
+ 
+            }catch(err) {
+            console.error(err);
+            return msg.channel.send('لم يتم إختيآر مقطع صوتي');
+            }
+          const videoIndex = parseInt(response.first().content);
+          var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+        } catch (err) {
+          console.error(err);
+          return msg.channel.send(':x: لا يتوفر نتآئج بحث ');
+        }
+    }
+ 
+      return handleVideo(video, msg, voiceChannel);
+    }
+  } else if (command === `skip`) {
+    if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
+    if (!serverQueue) return msg.channel.send('لا يتوفر مقطع لتجآوزه');
+    serverQueue.connection.dispatcher.end('تم تجآوز هذآ المقطع');
+    return undefined;
+  } else if (command === `stop`) {
+    if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
+    if (!serverQueue) return msg.channel.send('لا يتوفر مقطع لإيقآفه');
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end('تم إيقآف هذآ المقطع');
+    return undefined;
+  } else if (command === `vol`) {
+    if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
+    if (!serverQueue) return msg.channel.send('لا يوجد شيء شغآل.');
+    if (!args[1]) return msg.channel.send(`:loud_sound: مستوى الصوت **${serverQueue.volume}**`);
+    serverQueue.volume = args[1];
+    serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 50);
+    return msg.channel.send(`:speaker: تم تغير الصوت الي **${args[1]}**`);
+  } else if (command === `np`) {
+    if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
+    const embedNP = new Discord.RichEmbed()
+  .setDescription(`:notes: الان يتم تشغيل : **${serverQueue.songs[0].title}**`)
+    return msg.channel.sendEmbed(embedNP);
+  } else if (command === `replay`) {
+    if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
+    const embedNP = new Discord.RichEmbed()
+  .setDescription(`سيتم اعاده تشغيل الفديو :**${serverQueue.songs[0].title}**`)
+  msg.channel.send({embed: embedNP})
+     return handleVideo(video, msg, msg.member.voiceChannel);
+ 
+  } else if (command === `queue`) {
+    if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
+    let index = 0;
+    const embedqu = new Discord.RichEmbed()
+.setDescription(`**Songs Queue**
+${serverQueue.songs.map(song => `**${++index} -** ${song.title}`).join('\n')}
+**الان يتم تشغيل** ${serverQueue.songs[0].title}`)
+    return msg.channel.sendEmbed(embedqu);
+  } else if (command === `pause`) {
+    if (serverQueue && serverQueue.playing) {
+      serverQueue.playing = false;
+      serverQueue.connection.dispatcher.pause();
+      return msg.channel.send('تم إيقاف الموسيقى مؤقتا!');
+    }
+    return msg.channel.send('لا يوجد شيء حالي ف العمل.');
+  } else if (command === "resume") {
+    if (serverQueue && !serverQueue.playing) {
+      serverQueue.playing = true;
+      serverQueue.connection.dispatcher.resume();
+      return msg.channel.send('استأنفت الموسيقى بالنسبة لك !');
+    }
+    return msg.channel.send('لا يوجد شيء حالي في العمل.');
   }
-  //end of create role
-  let mutetime = args[1];
-  if(!mutetime) return message.reply("You didn't specify a time!");
-
-  await(tomute.addRole(muterole.id));
-  message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
-
-  setTimeout(function(){
-    tomute.removeRole(muterole.id);
-    message.channel.send(`<@${tomute.id}> has been unmuted!`);
-  }, ms(mutetime));
-
-
-//end of module
+ 
+  return undefined;
+async function handleVideo(video, msg, voiceChannel, playlist = false) {
+  const serverQueue = queue.get(msg.guild.id);
+  const song = {
+    id: video.id,
+    title: Util.escapeMarkdown(video.title),
+    url: `https://www.youtube.com/watch?v=${video.id}`,
+    time:`${video.duration.hours}:${video.duration.minutes}:${video.duration.seconds}`,
+    eyad:`${video.thumbnails.high.url}`,
+    best:`${video.channel.title}`,
+    bees:`${video.raw.snippet.publishedAt}`,
+    shahd:`${video.raw.kind}`,
+    zg:`${video.raw.snippet.channelId}`,
+        views:`${video.raw.views}`,
+        like:`${video.raw.likeCount}`,
+        dislike:`${video.raw.dislikeCount}`,
+        hi:`${video.raw.id}`
+  };
+  if (!serverQueue) {
+    const queueConstruct = {
+      textChannel: msg.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true
+    };
+    queue.set(msg.guild.id, queueConstruct);
+    queueConstruct.songs.push(song);
+    try {
+      var connection = await voiceChannel.join();
+      queueConstruct.connection = connection;
+      play(msg.guild, queueConstruct.songs[0]);
+    } catch (error) {
+      console.error(`I could not join the voice channel: ${error}`);
+      queue.delete(msg.guild.id);
+      return msg.channel.send(`لا أستطيع دخول هذآ الروم ${error}`);
+    }
+  } else {
+    serverQueue.songs.push(song);
+    console.log(serverQueue.songs);
+    if (playlist) return undefined;
+    else return msg.channel.send(` **${song.title}** تم اضافه الاغنية الي القائمة!`);
+  }
+  return undefined;
 }
-
-module.exports.help = {
-  name: "tempmute"
-}
+ 
+function play(guild, song) {
+  const serverQueue = queue.get(guild.id);
+ 
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+  console.log(serverQueue.songs);
+  const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+    .on('end', reason => {
+      if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+      else console.log(reason);
+      serverQueue.songs.shift();
+      play(guild, serverQueue.songs[0]);
+    })
+    .on('error', error => console.error(error));
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    fetchVideoInfo(`${song.hi}`, function (err,  idk) {
+  if (err) throw new Error(err);
+  console.log( idk);
+      const yyyy = {}
+  if(!yyyy[msg.guild.id]) yyyy[msg.guild.id] = {
+    like: `${ idk.likeCount}`,
+    dislike: `${ idk.dislikeCount}`
+  }
+  serverQueue.textChannel.send({embed : new Discord.RichEmbed()
+  .setTitle(`**${ idk.title}**`)
+  .setURL( idk.url)
+  .addField('Time The Video :' , `${song.time}`, true)
+  .addField('Channel Name :
