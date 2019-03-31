652d5ca0d05c14e4499c6c35400cddf96607ebf0
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-03-31 19:15:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '!'
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("k",{type: 'WATCHING'})
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -26,6 +27,8 @@ client.on('ready', () => {
   console.log('')
   console.log('')
 });
+ 
+client.login(process.env.BOT_TOKEN);
  
 const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
