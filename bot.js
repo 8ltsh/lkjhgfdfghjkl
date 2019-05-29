5e884176cb5f214e0d2e720520e314b1149f5e1d
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-05-29 16:13:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,15 @@
-onst Discord = require('discord.js');
+const Discord = require('discord.js');
+const moment = require("moment");
+const fs = require("fs");
 const client = new Discord.Client();
-const prefix = #
- 
+ const prefix = "-";
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+    console.log('I am ready!');
 });
- 
+
+client.on('ready', () => {
+    client.user.setActivity("HEY",{type: 'PLAYING'})
+
+});
+
 client.login(process.env.BOT_TOKEN);
