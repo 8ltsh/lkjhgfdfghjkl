6ba140a4bcefaffb3a96d7f7f85a7c581385df6b
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-05-29 11:54:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
 
 client.login(process.env.BOT_TOKEN); 
 
