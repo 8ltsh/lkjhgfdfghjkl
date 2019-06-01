8c11affb75a8d64bba23a2764924811feaffb214
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-06-01 16:30:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,13 +3,3 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-
-client.login(process.env.BOT_TOKEN);
