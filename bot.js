c915bd3c762013c7e1b35553f98741045840f388
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-06-02 15:34:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,3 +12,67 @@ client.on('message', msg => {
 });
 
 client.login(process.env.BOT_TOKEN);
+
+
+
+const devs = ['294075671020568578' , '' , ''];
+
+const adminprefix = "-";
+
+client.on('message', message => {
+
+    var argresult = message.content.split(` `).slice(1).join(' ');
+
+      if (!devs.includes(message.author.id)) return;
+
+      
+
+  if (message.content.startsWith(adminprefix + 'ply')) {
+
+    client.user.setGame(argresult);
+
+      message.channel.sendMessage(`**  ${argresult} ply .. ✅**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'wt')) {
+
+  client.user.setActivity(argresult, {type:'WATCHING'});
+
+      message.channel.sendMessage(`**  ${argresult} wt .. ✅**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'ls')) {
+
+  client.user.setActivity(argresult , {type:'LISTENING'});
+
+      message.channel.sendMessage(`**  ${argresult} ls .. ✅**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'li')) {
+
+    client.user.setGame(argresult, "https://www.twitch.tv/muuuuuute");
+
+      message.channel.sendMessage(`**  ${argresult} st .. ✅**`)
+
+    }
+
+  if (message.content.startsWith(adminprefix + 'us')) {
+
+  client.user.setUsername(argresult).then
+
+      message.channel.send(`**us ..**${argresult}** `)
+
+  } else
+
+  if (message.content.startsWith(adminprefix + 'ev')) {
+
+  client.user.setAvatar(argresult);
+
+    message.channel.send(`**evatar ... :** `);
+
+  }
+
+  });
