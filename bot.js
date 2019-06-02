cee3f84e596f01ef253fb5fafa7f1f35ad40062e
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-06-02 15:18:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,15 @@
-onst Discord = require('discord.js');
+const Discord = require('discord.js');
+const moment = require("moment");
+const fs = require("fs");
 const client = new Discord.Client();
+ const prefix = "-";
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity("HEY",{type: 'PLAYING'})
+
+});
+
+client.login(process.env.BOT_TOKEN);
